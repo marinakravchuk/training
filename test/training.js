058d84115ba477fe15b243a37dd1466cf3aac797
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2022-11-02 10:51:18 +0200

    Lesson1
    functions

---
+++ test/training.js
@@ -0,0 +1,73 @@
+/* let a = "test";
+
+
+if (a.length === 1) {
+    console.log("В слове одна буква")
+}
+else if(a.length === 2)  {
+    console.log("В слове 2 букв")
+}
+else{
+    console.log("В слове 3 букв и больше букв")
+} */
+
+/* let c = 4; */
+/* 
+if (c > 0){
+    console.log("число положительное ")
+}
+else{
+    console.log("число отрицательное ")
+}
+ */
+
+/* c > 0 ? console.log("число положительное ") : console.log("число отрицательное "); */
+
+/* let word = "cat";
+
+word.length === 10 ? console.log("Yes") : console.log("No"); */
+
+
+/* let number = 5;
+
+number < 0 ? number = Math.abs(number) : null
+
+console.log(number) */
+
+
+/* function test(str){
+let val = str + "!";
+    console.log(val);
+}
+
+function test2(str){
+    let val = str + "!";
+        return val;
+    } */
+    
+
+/*     function test3 (num1, num2){
+        return num1 + num2;
+      console.log("test");
+    }
+     */
+
+/*     function test4(num1, num2){
+        if(num1 > num2){
+        return num1
+        }
+        else{
+            return num2
+        }
+        }
+ */
+
+    /*     function test4(num1, num2){
+         return  num1 > num2 ? num1 : num2
+            }
+     */
+
+
+/*             function test5(val){
+                return typeof(val) === "string" ? "string" : "not string";
+            } */
